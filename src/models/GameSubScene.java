3bad49d0c69f551fc2474cb946fad638a79de165
alucard35 <bhambl3t0n1@gmail.com>
2019-04-20 00:11:54 -0600

    Added background image for subscene. Changed size and position a bit.

--- src/models/GameSubScene.java
+++ src/models/GameSubScene.java
@@ -2,20 +2,27 @@ package models;
 
 import javafx.animation.TranslateTransition;
 import javafx.scene.SubScene;
+import javafx.scene.image.Image;
 import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.Background;
+import javafx.scene.layout.BackgroundImage;
 import javafx.util.Duration;
 
 public class GameSubScene extends SubScene	{
 	
 	private boolean isHidden;
+	private final String BACKGROUND_PATH = "/models/resources/game_sub_scene.png";
 	
 	public GameSubScene() {
-		super(new AnchorPane(), 700, 500);
-		prefWidth(700);
-		prefHeight(500);
+		super(new AnchorPane(), 575, 700);
+		
+		prefWidth(575);
+		prefHeight(700);
 		
 		setLayoutX(300);
 		setLayoutY(1000);
+		
+		createBackground(); 
 		
 		isHidden = true;
 	}
@@ -25,13 +32,21 @@ public class GameSubScene extends SubScene	{
 		gameTransition.setDuration(Duration.seconds(0.6));
 		gameTransition.setNode(this);
 		if(isHidden) {
-			gameTransition.setToY(-950);
+			gameTransition.setToY(-993);
 			isHidden = false;
 		}
 		else {
-			gameTransition.setToY(950);
+			gameTransition.setToY(993);
 			isHidden = true;
 		}
 		gameTransition.play();
+	}
+	
+	private void createBackground() {
+		Image background = new Image(BACKGROUND_PATH);
+		BackgroundImage backgroundImage = new BackgroundImage(background, null, null, null, null);
+		Background anchorPaneBackground = new Background(backgroundImage);
+		AnchorPane root2 = (AnchorPane) this.getRoot();
+		root2.setBackground(anchorPaneBackground);
 	}
 }
