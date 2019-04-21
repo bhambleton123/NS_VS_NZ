8d07ec572c33903a68655369b756e7e2b779696b
Brian Hambleton <bhambl3t0n1@gmail.com>
2019-04-21 10:18:52 -0600

    Added more to ninja class

--- src/models/characters/Enemy.java
+++ src/models/characters/Enemy.java
@@ -1,5 +1,5 @@
 package models.characters;
 
 public interface Enemy {
-
+	
 }
--- src/models/characters/Ninja.java
+++ src/models/characters/Ninja.java
@@ -1,5 +1,61 @@
 package models.characters;
 
-public class Ninja {
+import javafx.scene.image.Image;
+
+public class Ninja implements Character {
+	
+	private double movementSpeed;
+	private double movementSpeedMultiplier;
+	
+	public Ninja(double movementSpeedInput, double movementSpeedMultiplierInput) {
+		this.movementSpeed = movementSpeedInput;
+		this.movementSpeedMultiplier = movementSpeedMultiplierInput;
+	}
+
+	@Override
+	public double getMovementSpeed() {
+		return movementSpeed;
+	}
+
+	@Override
+	public double getMovementSpeedMultiplier() {
+		return movementSpeedMultiplier;
+	}
+
+	@Override
+	public void movementAnimation() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void createWeapons() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void chooseWeapon() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public Image getCharacterImage() {
+		return null;
+		
+	}
+
+	@Override
+	public void changePlayerHealth() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void killCharacter() {
+		// TODO Auto-generated method stub
+		
+	}
 
 }
